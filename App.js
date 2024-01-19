6b6d84ae0066285c2e845d517b9c812493b8df74
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-01-19 23:17:30 +0100

    update

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import 'react-native-gesture-handler';
+
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
