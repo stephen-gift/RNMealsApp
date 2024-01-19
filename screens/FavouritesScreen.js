a50d582494108f518ce2772ed269cc2e053d8c23
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-01-20 00:05:39 +0100

    update

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -10,7 +10,26 @@ export default function FavouritesScreen() {
   const favourMeals = MEALS.filter((meal) =>
     favouriteMealsCtx.id.includes(meal.id)
   );
+
+  if (favourMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.title}>You have no favourites meal yet</Text>
+      </View>
+    );
+  }
   return <MealList displayedMeals={favourMeals} />;
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color: "white",
+  },
+});
