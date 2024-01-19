f36d3a20b2327e1c64d3dfadbbe93f8581eae932
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-01-19 07:38:58 +0100

    update

--- App.js
+++ App.js
@@ -3,12 +3,12 @@ import { StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 
 export default function App() {
-  return 
-  <>
-  <StatusBar style="light"/>
-  <CategoriesScreen />;
-
-  </>
+  return (
+    <>
+      <StatusBar style="light" />
+      <CategoriesScreen />
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
